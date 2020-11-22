00dd88852c3a971274701eb1c08b31bb8c7c7844
Matheus Azevedo <matheus.azevedo.91@gmail.com>
2020-11-22 14:57:14 -0300

    API de desenvolvedores v1.0

--- app.py
+++ app.py
@@ -1,16 +1,52 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import json
 
 app = Flask(__name__)
 
 desenvolvedores = [
 
-    {"nome":"Matheus",
+    {   "id":"0",
+        "nome":"Matheus",
      "habilidades":["Python", "Flask"]},
-    {"nome":"Azevedo",
+    {    "id":"1",
+        "nome":"Azevedo",
      "habilidades":["Python", "Django"]}
 ]
-@app.route("/dev")
-def desenvolvedor():
-    return jsonify({"name":"Matheus"})
+
+# Devolve, atualiza ou deleta um desenvolvedor por ID
+@app.route("/dev/<int:id>/", methods=["GET", "PUT", "DELETE"])
+def desenvolvedor(id):
+
+    if request.method == "GET":
+        try:
+            response = desenvolvedores[id]
+        except IndexError:
+            response = {"status":"Erro", "mensagem":f"desenvolvedor de ID {id} não existe"}
+        except Exception:
+            response = {"status":"Erro", "mensagem":"mensagem de erro desconhecido, procure o ADM"}
+        return jsonify(response)
+
+    elif request.method == "PUT":
+        dados = json.loads(request.data)
+        desenvolvedores[id] = dados
+        return jsonify(dados)
+
+    elif request.method == "DELETE":
+        desenvolvedores.pop(id)
+        return jsonify({"status":"sucesso", "mensagem":"Registro excluído!"})
+
+# Lista todos os desenvolvedores e inclui um novo desenvolvedor
+@app.route("/dev/", methods=["GET", "POST"])
+def lista_desenvolvedores():
+    if request.method == "POST":
+        dados = json.loads(request.data)
+        posicao = len(desenvolvedores)
+        dados['id'] = posicao
+        desenvolvedores.append(dados)
+        return jsonify({"status":"sucesso", "mensagem":"registro inserido"})
+
+    elif request.method == "GET":
+        return jsonify(desenvolvedores)
+
 if __name__ == '__main__':
     app.run(debug=True)
