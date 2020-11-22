aa6b021faaf5eb44ef144789285e0b2ad140767d
Matheus Azevedo <matheus.azevedo.91@gmail.com>
2020-11-22 14:00:51 -0300

    salvando cadastros de devs

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+desenvolvedores = [
+
+    {"nome":"Matheus",
+     "habilidades":["Python", "Flask"]},
+    {"nome":"Azevedo",
+     "habilidades":["Python", "Django"]}
+]
 @app.route("/dev")
 def desenvolvedor():
     return jsonify({"name":"Matheus"})
